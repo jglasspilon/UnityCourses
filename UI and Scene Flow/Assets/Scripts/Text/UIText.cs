dfabba65fb7080fb128746ec7910a0d5a4fcf736
jglasspilon <jglasspilon@live.com>
2017-12-19 13:06:23 -0500

    Finished simple text box system (instant display of text). WIll work on animated text (gradual display) next

---
+++ UI and Scene Flow/Assets/Scripts/Text/UIText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//base class for all text UI elements
+public abstract class UIText : MonoBehaviour {
+
+    //text sheet that holds the text data for the current text box
+    protected TextSheet textSheet;
+
+    /// <summary>
+    /// Updates the text with a new sheet
+    /// </summary>
+    /// <param name="text">new text sheet to set</param>
+    public virtual void UpdateTextSheet(TextSheet newText)
+    {
+        textSheet = newText;
+    }
+
+    /// <summary>
+    /// Cycles to the next text in the text collection
+    /// </summary>
+    public abstract void CycleText();
+}
---
+++ UI and Scene Flow/Assets/Scripts/Text/UIText_Simple.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Extends the base UIText class and displays each string from the text sheet one at a time instantly
+public class UIText_Simple : UIText {
+
+    //related to the text component
+    private Text text;                      //actual text to be printed in the text box
+    private CanvasGroup textGroup;          //canvas group that controls the text box visibility and interactability
+    private int textIndex = 0;              //keeps track of the current text within the text collection
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        textGroup = GetComponent<CanvasGroup>();
+    }
+
+    public override void UpdateTextSheet(TextSheet newText)
+    {
+        base.UpdateTextSheet(newText);
+
+        //initialize the text component with the new text string
+        textIndex = 0;
+        text.text = textSheet.textCollection[textIndex];
+
+        //open the text box
+        textGroup.alpha = 1;
+        textGroup.blocksRaycasts = true;
+    }
+
+    public override void CycleText()
+    {
+        //if there is no current text sheet then we can't cycle
+        if(textSheet == null)
+        {
+            return;
+        }
+
+        else
+        {
+            //if we have more texts to go through than go to the next text
+            if(textIndex < textSheet.textCollection.Length - 1)
+            {
+                textIndex++;
+                text.text = textSheet.textCollection[textIndex];
+            }
+
+            //otherwise we are at the end of the text sheet so close the text box
+            else
+            {
+                textGroup.alpha = 0;
+                textGroup.blocksRaycasts = false;
+            }
+        }
+    }
+}
